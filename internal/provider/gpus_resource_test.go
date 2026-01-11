ef5b4d8e15f0f72e51d0acfc28a53fdd49d3d87b
jtbaldwi@gmail.com <jtbaldwi@gmail.com>
2026-01-10 19:16:05 -0500

    test: Add acceptance tests for gpus

---
+++ internal/provider/gpus_resource_test.go
@@ -0,0 +1,214 @@
+package provider
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+)
+
+var gpcnGPUTest = "gpcn_gpu.test"
+
+func TestGPUResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: providerConfig + `
+data "gpcn_datacenters" "east_us" {
+  country_name = "United States"
+  region_name  = "east"
+}
+
+resource "gpcn_gpu" "test" {
+  name          = "terraform-demo-gpu"
+  datacenter_id = data.gpcn_datacenters.east_us.datacenters[0].id
+  series_name   = "A100 Series"
+  gpu_count     = 1
+}
+`,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						// Expect initial create action
+						plancheck.ExpectResourceAction(gpcnGPUTest, plancheck.ResourceActionCreate),
+					},
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify computed attributes are set
+					resource.TestCheckResourceAttrSet(gpcnGPUTest, "id"),
+					resource.TestCheckResourceAttrSet(gpcnGPUTest, "created_time"),
+					resource.TestCheckResourceAttrSet(gpcnGPUTest, "last_updated"),
+					// Verify series_code was computed from series_name
+					resource.TestCheckResourceAttr(gpcnGPUTest, "series_code", "a100_series"),
+					// Verify location map is populated
+					resource.TestCheckResourceAttrSet(gpcnGPUTest, "location.datacenter"),
+					resource.TestCheckResourceAttrSet(gpcnGPUTest, "location.region"),
+					resource.TestCheckResourceAttrSet(gpcnGPUTest, "location.country"),
+					// Verify configured attributes
+					resource.TestCheckResourceAttr(gpcnGPUTest, "name", "terraform-demo-gpu"),
+					resource.TestCheckResourceAttr(gpcnGPUTest, "datacenter_id", "1ea6b709-0671-46fa-aea8-bdc8eb897d3d"),
+					resource.TestCheckResourceAttr(gpcnGPUTest, "series_name", "A100 Series"),
+					resource.TestCheckResourceAttr(gpcnGPUTest, "gpu_count", "1"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName: gpcnGPUTest,
+				ImportState:  true,
+			},
+			// Update and Read testing (name change)
+			{
+				Config: providerConfig + `
+data "gpcn_datacenters" "east_us" {
+  country_name = "United States"
+  region_name  = "east"
+}
+
+resource "gpcn_gpu" "test" {
+  name          = "terraform-demo-gpu-updated"
+  datacenter_id = data.gpcn_datacenters.east_us.datacenters[0].id
+  series_name   = "A100 Series"
+  gpu_count     = 1
+}
+`,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						// Expect update action for name change
+						plancheck.ExpectResourceAction(gpcnGPUTest, plancheck.ResourceActionUpdate),
+					},
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify name has been updated
+					resource.TestCheckResourceAttr(gpcnGPUTest, "name", "terraform-demo-gpu-updated"),
+				),
+			},
+		},
+	})
+}
+
+func TestGPUResourceInvalidSeries(t *testing.T) {
+	t.Run("invalid_series_code", func(t *testing.T) {
+		resource.UnitTest(t, resource.TestCase{
+			ProtoV6ProviderFactories: testProtoV6ProviderFactories,
+			Steps: []resource.TestStep{
+				{
+					Config: providerConfig + `
+resource "gpcn_gpu" "test" {
+  name          = "terraform-gpu-bad-code"
+  datacenter_id = "1ea6b709-0671-46fa-aea8-bdc8eb897d3d"
+  series_code   = "invalid_series_code"
+  gpu_count     = 1
+}
+`,
+					ExpectError: regexp.MustCompile("Attribute series_code value must be one of"),
+				},
+			},
+		})
+	})
+
+	t.Run("invalid_series_name", func(t *testing.T) {
+		resource.UnitTest(t, resource.TestCase{
+			ProtoV6ProviderFactories: testProtoV6ProviderFactories,
+			Steps: []resource.TestStep{
+				{
+					Config: providerConfig + `
+resource "gpcn_gpu" "test" {
+  name          = "terraform-gpu-bad-series"
+  datacenter_id = "1ea6b709-0671-46fa-aea8-bdc8eb897d3d"
+  series_name   = "Invalid GPU Series"
+  gpu_count     = 1
+}
+`,
+					ExpectError: regexp.MustCompile("Attribute series_name value must be one of"),
+				},
+			},
+		})
+	})
+
+	t.Run("both_code_and_name", func(t *testing.T) {
+		resource.UnitTest(t, resource.TestCase{
+			ProtoV6ProviderFactories: testProtoV6ProviderFactories,
+			Steps: []resource.TestStep{
+				{
+					Config: providerConfig + `
+resource "gpcn_gpu" "test" {
+  name          = "terraform-gpu-both-series"
+  datacenter_id = "1ea6b709-0671-46fa-aea8-bdc8eb897d3d"
+  series_name   = "A100 Series"
+  series_code   = "a100_series"
+  gpu_count     = 1
+}
+`,
+					ExpectError: regexp.MustCompile("2 attributes specified"),
+				},
+			},
+		})
+	})
+
+	t.Run("neither_code_nor_name", func(t *testing.T) {
+		resource.UnitTest(t, resource.TestCase{
+			ProtoV6ProviderFactories: testProtoV6ProviderFactories,
+			Steps: []resource.TestStep{
+				{
+					Config: providerConfig + `
+resource "gpcn_gpu" "test" {
+  name          = "terraform-gpu-no-series"
+  datacenter_id = "1ea6b709-0671-46fa-aea8-bdc8eb897d3d"
+  gpu_count     = 1
+}
+`,
+					ExpectError: regexp.MustCompile("No attribute specified when one"),
+				},
+			},
+		})
+	})
+}
+
+func TestGPUResourceInvalidGPUCount(t *testing.T) {
+	t.Run("gpu_count_zero", func(t *testing.T) {
+		resource.UnitTest(t, resource.TestCase{
+			ProtoV6ProviderFactories: testProtoV6ProviderFactories,
+			Steps: []resource.TestStep{
+				{
+					Config: providerConfig + `
+resource "gpcn_gpu" "test" {
+  name          = "terraform-gpu-invalid-count"
+  datacenter_id = "1ea6b709-0671-46fa-aea8-bdc8eb897d3d"
+  series_name   = "A100 Series"
+  gpu_count     = 0
+}
+`,
+					ExpectError: regexp.MustCompile("Attribute gpu_count value must be one of"),
+				},
+			},
+		})
+	})
+}
+
+func TestGPUResourceNoAvailability(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + `
+data "gpcn_datacenters" "east_us" {
+  country_name = "United States"
+  region_name  = "east"
+}
+
+resource "gpcn_gpu" "test" {
+  name          = "terraform-demo-gpu-no-availability"
+  datacenter_id = data.gpcn_datacenters.east_us.datacenters[0].id
+  series_code   = "a100_series"
+  gpu_count     = 4
+}
+`,
+				// This should fail due to insufficient GPU availability
+				// The exact error message depends on the API response
+				ExpectError: regexp.MustCompile("(No GPU availability|No GPU Inventory Available|Unable to create GPCN GPU)"),
+			},
+		},
+	})
+}
